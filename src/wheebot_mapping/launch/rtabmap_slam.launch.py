2800677a5d0c040288a0a245070b203fc1fdf4ea
therafaeliger <rafaeliger.hadikusuma@gmail.com>
2025-12-12 10:01:56 +0700

    add .db saving mechanism

--- src/wheebot_mapping/launch/rtabmap_slam.launch.py
+++ src/wheebot_mapping/launch/rtabmap_slam.launch.py
@@ -6,10 +6,17 @@ from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+import datetime
 
 def generate_launch_description():
     # wheebot_mapping_dir = get_package_share_directory("wheebot_mapping")
     rtabmap_launch_dir = get_package_share_directory("rtabmap_launch")
+
+    # Dynamic Database Path
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    db_dir = "/home/rafael/Documents/dbdb"
+    os.makedirs(db_dir, exist_ok=True)
+    db_path = os.path.join(db_dir, f"rtabmap_{timestamp}.db")
 
     rtabmap_launch = IncludeLaunchDescription(
         os.path.join(rtabmap_launch_dir, "launch", "rtabmap.launch.py"),
@@ -17,7 +24,7 @@ def generate_launch_description():
             'use_sim_time': 'false',
 
             # 'args': '--delete_db_on_start',
-            # 'args': '--database_path ~/.ros/rtabmap.db',
+            # 'args': '--database_path ~/.ros/rtabmap.db', # untuk lokalisasi
             'args': '--delete_db_on_start \
                     --Grid/MinGroundHeight -0.4 \
                     --Grid/MaxGroundHeight 0.4 \
@@ -41,7 +48,7 @@ def generate_launch_description():
             'map_topic': 'map',
             'publish_tf_map': 'true',
             'namespace': 'rtabmap',
-            # 'database_path': '~/.ros/rtabmap.db',
+            'database_path': {db_path},
             'topic_queue_size': '10',
             'queue_size': '10',
             'wait_for_transform': '1.0',
